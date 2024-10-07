99b46484334d5e5531bc5f2e94325e7bc4ef058b
vjalby <vincent.jalby@orange.fr>
2024-10-07 18:09:28 +0200

    Version 0.2.2 (with french translation)

--- build.R
+++ build.R
@@ -11,3 +11,15 @@ jmvtools::addAnalysis(name='likertplot', title='Likert Plot') # name = function/
 
 jmvtools::install()
 
+
+
+## i18n
+
+# Find the corect path
+node::node()
+#/Library/Frameworks/R.framework/Versions/4.4-arm64/Resources/library/node/node-darwin/bin/node
+jmvtools:::jmcPath()
+# /Library/Frameworks/R.framework/Versions/4.4-arm64/Resources/library/jmvtools/node_modules/jamovi-compiler/index.js
+
+# Command (terminal) to generate i18n
+/Library/Frameworks/R.framework/Versions/4.4-arm64/Resources/library/node/node-darwin/bin/node /Library/Frameworks/R.framework/Versions/4.4-arm64/Resources/library/jmvtools/node_modules/jamovi-compiler/index.js --i18n ./  --create fr
